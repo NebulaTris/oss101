16827ab57aa8e515e0e423fc10ed1c470f6d4640
nitinya9av <nitinya9av@gmail.com>
2024-01-06 15:22:02 +0530

    reCheck_linting

--- contributors/nebulatris/nitinya9av/level2_task6.py
+++ contributors/nebulatris/nitinya9av/level2_task6.py
@@ -11,7 +11,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Error! Division by zero(0) not defined."
+        return "Error! Division by zero not defined."
     else:
         return x / y
 
